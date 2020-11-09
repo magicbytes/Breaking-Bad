6d0111f520c3916ea145d8dfcb81398c6ec790c2
Alex <cebotari.alex@gmail.com>
2020-11-09 01:17:32 +0000

    Add filters for season

--- app/src/main/java/com/magicbytes/breakingbad_interviewtask/features/charactersList/BreakingBadCharactersActivity.kt
+++ app/src/main/java/com/magicbytes/breakingbad_interviewtask/features/charactersList/BreakingBadCharactersActivity.kt
@@ -2,12 +2,17 @@ package com.magicbytes.breakingbad_interviewtask.features.charactersList
 
 import BreakingBadCharactersAdapter
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.magicbytes.breakingbad_interviewtask.R
 import com.magicbytes.breakingbad_interviewtask.domain.BreakingBadCharacter
 import com.magicbytes.breakingbad_interviewtask.features.characterDetails.BreakingBadCharactersDetailsActivity
 import kotlinx.android.synthetic.main.activity_breaking_bad_characters.*
+
 
 class BreakingBadCharactersActivity : AppCompatActivity() {
 
@@ -24,12 +29,57 @@ class BreakingBadCharactersActivity : AppCompatActivity() {
         listenViewModel()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_breaking_bad_characters, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_filter -> {
+                viewModel.showFilterOptions()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
     private fun listenViewModel() {
         viewModel.isLoading.observe(this) {
             swipeToRefreshLayout.isRefreshing = it
         }
         viewModel.characters.observe(this) {
             adapter.breakingBadCharacters = it
+        }
+        viewModel.currentFilter.observe(this) { seasonNumber ->
+            title = if (seasonNumber == -1) {
+                "Characters - All seasons"
+            } else {
+                "Characters - Season $seasonNumber"
+            }
+        }
+        viewModel.filterOptions.observe(this) { filters ->
+            if (filters.isEmpty()) {
+                Toast.makeText(this, "There are no filter to select from...", Toast.LENGTH_SHORT).show()
+            } else {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Choose season")
+
+                val options = filters.map { "Season $it" }.toMutableList()
+                options.add(0, "All Seasons")
+
+                builder.setItems(options.toTypedArray()) { _, which ->
+                    if (which == 0) {
+                        viewModel.filterBy(-1)
+                    } else {
+                        viewModel.filterBy(filters[which - 1])
+                    }
+                }
+
+                val dialog = builder.create()
+                dialog.show()
+            }
         }
     }
 
--- app/src/main/java/com/magicbytes/breakingbad_interviewtask/features/charactersList/BreakingBadCharactersViewModel.kt
+++ app/src/main/java/com/magicbytes/breakingbad_interviewtask/features/charactersList/BreakingBadCharactersViewModel.kt
@@ -12,15 +12,24 @@ import kotlinx.coroutines.launch
 class BreakingBadCharactersViewModel(private val charactersRepo: BreakingBadCharactersRepository) : ViewModel() {
 
     private var allFoundCharacters: List<BreakingBadCharacter> = emptyList()
+    private var maxSeasonsNumber = 0
 
     private val _characters = MutableLiveData<List<BreakingBadCharacter>>()
     private val _isLoading = MutableLiveData<Boolean>()
+    private val _filterOptions = MutableLiveData<List<Int>>()
+    private val _currentFilter = MutableLiveData<Int>()
+
+    val currentFilter: LiveData<Int>
+        get() = _currentFilter
 
     val characters: LiveData<List<BreakingBadCharacter>>
         get() = _characters
 
     val isLoading: LiveData<Boolean>
         get() = _isLoading
+
+    val filterOptions: LiveData<List<Int>>
+        get() = _filterOptions
 
     init {
         loadAllCharacters()
@@ -30,6 +39,7 @@ class BreakingBadCharactersViewModel(private val charactersRepo: BreakingBadChar
      * If -1 return all characters from all seasons, otherwise all characters specific for the [seasonId]
      */
     fun filterBy(seasonId: Int) {
+        _currentFilter.postValue(seasonId)
         viewModelScope.launch(Dispatchers.IO) {
             val characters = if (seasonId < 0) {
                 allFoundCharacters
@@ -42,10 +52,18 @@ class BreakingBadCharactersViewModel(private val charactersRepo: BreakingBadChar
         }
     }
 
+    fun showFilterOptions() {
+        val availableOptions = (1 until maxSeasonsNumber).map { it }
+        _filterOptions.value = availableOptions
+    }
+
     private fun loadAllCharacters() {
         _isLoading.value = true
         viewModelScope.launch(Dispatchers.IO) {
             allFoundCharacters = charactersRepo.getAll()
+            maxSeasonsNumber = allFoundCharacters.map { it.seasonAppearance }.flatten().toSet().maxOrNull() ?: 0
+            _currentFilter.postValue(-1)
+
             _isLoading.postValue(false)
             _characters.postValue(allFoundCharacters)
         }
